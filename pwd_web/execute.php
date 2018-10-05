1fccf08b33492a5c78790caac639779250d8c88c
Wang Jihe <1321942045@qq.com>
2018-10-05 23:01:43 +0800

    2018.10.05 23:01
    Fix execute.php

--- pwd_web/execute.php
+++ pwd_web/execute.php
@@ -2,14 +2,23 @@
 	session_start();
 	include "settings.php";
 	if ($_SESSION["level"] != $problem_count)
-	{
-		echo "You have not finish the problem yet!";
-		return "f**k";
-	}
+		die("You have not finish the problem yet");
+
 	if ($_POST["CSRF"]!=$_SESSION["csrf_token_name"])
+		die("Cross-site Request Forgery Detected");
+
+	$arg[0]=$_POST["Text"];
+	$arg[1]=$_POST["Passwd"];
+	$arg[2]=$_POST["Security"];
+	$arg[3]=$_POST["Length"];
+	$arg[4]=$_POST["Type"];
+	$cmd="/usr/bin/passwdg ";
+	for ($i = 0; $i < 5; $i ++)
 	{
-		echo "Cross-site Request Forgery Detected!";
-		return "f**k";
+		if (strpos($arg[$i], "'") !== false)
+			die("Texts {$i} include ', but it is not allowed");
+		$arg[$i]="'".$arg[$i]."'";
+		$cmd=$cmd." ".$arg[$i];
 	}
-	echo shell_exec("../cryptopp/main {$_POST["Text"]} {$_POST["Passwd"]} {$_POST["Security"]} {$_POST["Length"]} {$_POST["Type"]}");
+	echo shell_exec($cmd);
 ?>
